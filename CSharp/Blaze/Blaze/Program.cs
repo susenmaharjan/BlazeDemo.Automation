a944eec73f3b87c0c5745d887e8c8522b9774e28
Susen <susen.maharjan@javra.com>
2019-05-03 15:05:49 +0545

    Project creation

---
+++ CSharp/Blaze/Blaze/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
